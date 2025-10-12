9416c53c3143cbd814a34b11f7a348392238a6b4
Bruno Serrate <brunostj@hotmail.com>
2025-10-12 05:45:37 -0300

    feat: rota da api de prod alterada

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:3000/api'
-    : 'https://api-pardoxandria.devstao.dev/api';
+    : 'https://pardoxandria-api.devstao.dev/';
 
 const DEFAULT_HEADERS = {
     'Content-Type': 'application/json',
