19a88b4067d9e5381cdeb3fb114cacd79fc62ffe
Bruno Serrate <brunostj@hotmail.com>
2025-10-21 07:00:11 -0300

    fix: ajuste na rota da API (config.js)

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:3000/api'
-    : 'https://pardoxandria-api.devstao.dev/';
+    : 'https://pardoxandria-api.devstao.dev';
 
 const DEFAULT_HEADERS = {
     'Content-Type': 'application/json',
